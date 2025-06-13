869762c2171345e5ed74daead9e36459ff42fd67
Jennifer-tech <muofunanyajennifer1@gmail.com>
2025-06-13 15:28:30 +0100

    rounded up the auth route and created new folders

--- auth-service/src/routes/authRoute.js
+++ auth-service/src/routes/authRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser} = require('../controllers/authController')
+const { registerUser, loginUser, refreshTokenUser, logoutUser} = require('../controllers/authController')
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser)
+router.post("/refresh-token", refreshTokenUser)
+router.post("/logout", logoutUser)
 
 module.exports = router;
